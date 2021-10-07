ca41afbcf9636fd0fad2c653b122b883e02c9ff4
Gabriel-Valin <gabrielvalincontato@gmail.com>
2021-10-07 13:53:57 -0300

    chore: add jest and ts-node-dev

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  colletCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
